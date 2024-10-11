09f914629d36a7fbd6016217eca270bf3e609eaf
Donghyun Rhee <drhee@cx2r7s6.42seoul.kr>
2024-10-11 17:17:11 +0900

    cpp00-ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cctype>
+
+int main(int ac, char **av)
+{
+	int j;
+	char c;
+
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			j = 0;
+			while (av[i][j])
+			{
+				c = av[i][j];
+				std::cout << (char)toupper(c);
+				j++;
+			}
+		}
+	}
+	return 0;
+}
