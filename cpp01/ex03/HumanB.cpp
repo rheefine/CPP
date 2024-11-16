219a03d0a59ddf421ef532c952c72e2d2f473076
Donghyun Rhee <drhee@cx2r7s5.42seoul.kr>
2024-11-16 21:44:12 +0900

    cpp01-ex03

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -1,6 +1,6 @@
 #include "HumanB.hpp"
 
-HumanB::HumanB(const std::string name) : name(name) {}
+HumanB::HumanB(const std::string name) : name(name), weapon(nullptr) {}
 
 void HumanB::setWeapon(Weapon& weapon) {
 	this->weapon = &weapon;
