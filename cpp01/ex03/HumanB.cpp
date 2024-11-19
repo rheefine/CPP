917d0fa46092c8aeb5f01b9389d78ca67a6eebfb
Donghyun Rhee <drhee@cx2r7s2.42seoul.kr>
2024-11-19 15:52:17 +0900

    cpp01

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -1,11 +1,15 @@
 #include "HumanB.hpp"
 
-HumanB::HumanB(const std::string name) : name(name), weapon(nullptr) {}
+HumanB::HumanB(const std::string name) : name(name), weapon(NULL) {}
 
 void HumanB::setWeapon(Weapon& weapon) {
 	this->weapon = &weapon;
 }
 
 void HumanB::attack() {
+	if (this->weapon == NULL) {
+		std::cout << this->name << " has no weapon" << std::endl << std::endl;
+		return;
+	}
 	std::cout << this->name << " attacks with " << this->weapon->getType() << std::endl << std::endl;
 }
--- cpp01/ex03/main.cpp
+++ cpp01/ex03/main.cpp
@@ -13,7 +13,6 @@ int main() {
 	{
 		Weapon club = Weapon("crude spiked club");
 		HumanB jim("Jim");
-		jim.setWeapon(club);
 		jim.attack();
 		club.setType("some other type of club");
 		jim.attack();
